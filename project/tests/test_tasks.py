f69ecbf9a5986f9e7fa97c0cc48d4fac1974233e
Dmitry Moroz <dimasbka@gmail.com>
2024-05-16 21:52:44 +0300

    ladded tests

--- project/tests/test_tasks.py
+++ project/tests/test_tasks.py
@@ -1,4 +1,4 @@
-from worker import create_task
+from worker import download_file_url, DOWNLOADS_DIR
 from unittest.mock import patch
 
 import json
@@ -9,40 +9,42 @@ def test_home(test_app):
     assert response.status_code == 200
 
 
-#def test_task():
-#    assert create_task.run(1)
-#    assert create_task.run(2)
-#    assert create_task.run(3)
-#    
-    
-    
+TEST_URL = "https://i.ytimg.com/vi/G4MlZK8fd44/hq720.jpg"
+TEST_RESULT = DOWNLOADS_DIR + "hq720.jpg"
+
+
 @patch("worker.create_task.run")
 def test_mock_task(mock_run):
-    assert create_task.run(1)
-    create_task.run.assert_called_once_with(1)
+    assert download_file_url.run(TEST_URL)
+    download_file_url.run.assert_called_once_with(1)
 
-    assert create_task.run(2)
-    assert create_task.run.call_count == 2
+    assert download_file_url.run(TEST_URL)
+    assert download_file_url.run.call_count == 2
 
-    assert create_task.run(3)
-    assert create_task.run.call_count == 3
-    
-    
+    assert download_file_url.run(TEST_URL)
+    assert download_file_url.run.call_count == 3
+
+
 def test_task_status(test_app):
-    response = test_app.post(
-        "/tasks",
-        data=json.dumps({"type": 1})
-    )
+    response = test_app.post("/download/file", data=json.dumps({"url": TEST_URL}))
     content = response.json()
     task_id = content["task_id"]
     assert task_id
 
     response = test_app.get(f"tasks/{task_id}")
     content = response.json()
-    assert content == {"task_id": task_id, "task_status": "PENDING", "task_result": None}
+    assert content == {
+        "task_id": task_id,
+        "task_status": "PENDING",
+        "task_result": None,
+    }
     assert response.status_code == 200
 
     while content["task_status"] == "PENDING":
         response = test_app.get(f"tasks/{task_id}")
         content = response.json()
-    assert content == {"task_id": task_id, "task_status": "SUCCESS", "task_result": True}
+    assert content == {
+        "task_id": task_id,
+        "task_status": "SUCCESS",
+        "task_result": TEST_RESULT,
+    }
